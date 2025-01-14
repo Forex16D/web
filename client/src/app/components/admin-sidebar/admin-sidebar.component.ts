afbe21ea05d5b4ab09e3609984893e9bdebbebf8
Nonz007x <comandor969@gmail.com>
2025-01-14 21:47:08 +0700

    fixed sidebar

--- client/src/app/components/admin-sidebar/admin-sidebar.component.ts
+++ client/src/app/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { RouterLink } from '@angular/router';
+import { DrawerModule } from 'primeng/drawer';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [LogoComponent, RouterLink],
+  imports: [LogoComponent, RouterLink, DrawerModule, DividerModule],
   templateUrl: './admin-sidebar.component.html',
-  styleUrl: './admin-sidebar.component.css'
+  styleUrl: './admin-sidebar.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AdminSidebarComponent {
-
-  hello() {
-    console.log("chemical");
-  }
+  visible = false;
 }
--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { AdminSidebarComponent } from '../../components/admin-sidebar/admin-sidebar.component';
+import { DrawerModule } from 'primeng/drawer';
+import { DividerModule } from 'primeng/divider';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-admin',
-  imports: [AdminSidebarComponent],
+  imports: [AdminSidebarComponent, DrawerModule, DividerModule, MenuModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
+  sideBarVisible = true;
 }
