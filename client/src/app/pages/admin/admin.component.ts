789f9b6ceb841c5350451b863dc4033ebc751d6a
Nonz007x <comandor969@gmail.com>
2025-01-16 14:53:19 +0700

    fixed sidebar

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -4,13 +4,20 @@ import { DrawerModule } from 'primeng/drawer';
 import { DividerModule } from 'primeng/divider';
 import { MenuModule } from 'primeng/menu';
 import { LogoComponent } from '../../components/logo/logo.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [AdminSidebarComponent, DrawerModule, DividerModule, MenuModule, LogoComponent],
+  imports: [AdminSidebarComponent,
+    DrawerModule,
+    DividerModule,
+    MenuModule,
+    LogoComponent,
+    NgIf],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-  sideBarVisible = true;
+  isVisible = true;
+  isMouseOver = false;
 }
